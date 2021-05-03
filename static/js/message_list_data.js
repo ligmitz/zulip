5cb55f1a13edb49df9b91e462c12583af3c00573
Gaurav Pandey <gauravguitarrocks@gmail.com>
2021-05-04 00:30:54 +0530

    message_list_data: Check if list is empty for first_unread_message_id.
    It is necessary to check if the message list is
    empty before returning this.last.id() under
    first_unread_message_id which would otherwise
    give 'this.last is undefined' error.

--- static/js/message_list_data.js
+++ static/js/message_list_data.js
@@ -216,7 +216,10 @@ export class MessageListData {
         }
 
         // if no unread, return the bottom message
-        return this.last().id;
+        if (!this.empty()) {
+            return this.last().id;
+        }
+        return undefined;
     }
 
     add_messages(messages) {
